43891e83622152bca7ae378d9fdc20accabf4884
nemliProg <younesbuzz@gmail.com>
2022-09-15 22:04:03 +0000

    some animation added with framer-motion pacha

--- feedback-app/src/components/FeedBackList.jsx
+++ feedback-app/src/components/FeedBackList.jsx
@@ -1,17 +1,29 @@
-import FeedBackItem from "./FeedBackItem"
+import { motion, AnimatePresence } from "framer-motion"
+import FeedBackItem from "./FeedBackItem";
 
-function FeedBackList({ feedback, handleDelete}) {
+function FeedBackList({ feedback, handleDelete }) {
   if (!feedback || feedback.length === 0) {
-    return <p className="text-red-600 font-bold text-center my-7">No Feedback Yet</p>
+    return (
+      <p className="text-red-600 font-bold text-center my-7">No Feedback Yet</p>
+    );
   }
 
-
   return <div className="m-4">
-      { feedback.map((item) => {
-        return <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
-      }) }
-      </div>
-  
+    <AnimatePresence>
+      {feedback.map((item) => {
+        return (
+          <motion.div 
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedBackItem key={item.id} item={item} handleDelete={handleDelete} />
+          </motion.div>
+        );
+      })}
+    </AnimatePresence>
+  </div>;
 }
 
-export default FeedBackList
+export default FeedBackList;
