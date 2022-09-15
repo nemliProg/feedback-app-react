ee002ad821a99e122d5366566db0f68818a35ba7
nemliProg <younesbuzz@gmail.com>
2022-09-15 21:39:38 +0000

    add Feedback feature

--- feedback-app/src/App.js
+++ feedback-app/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedBackStats from "./components/FeedBackStats";
 import FeedBackData from "./data/FeedBackData";
@@ -9,23 +10,29 @@ function App() {
   const [feedback, setFeedback] = useState(FeedBackData);
 
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure you want to delete?')) {
-      setFeedback( feedback.filter((item)=>{
-        return item.id !== id
-      }))
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(
+        feedback.filter((item) => {
+          return item.id !== id;
+        })
+      );
     }
+  };
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback,...feedback]);
+
   };
 
   return (
     <>
       <Header />
       <main className="container mx-auto">
-        <FeedBackForm />
+        <FeedBackForm handleAdd={addFeedback} />
         <FeedBackStats feedback={feedback} />
         <FeedBackList feedback={feedback} handleDelete={deleteFeedback} />
       </main>
     </>
   );
 }
-
 export default App;
--- feedback-app/src/components/FeedBackForm.jsx
+++ feedback-app/src/components/FeedBackForm.jsx
@@ -1,33 +1,47 @@
 import { useState } from "react";
 import RatingSelect from "./RatingSelect";
 
-function FeedBackForm() {
+function FeedBackForm({handleAdd}) {
 
-const [review, setReview] = useState('');
+const [text, setText] = useState('');
 const [isdisabled, setisdIsabled] = useState(true);
 const [message, setMessage] = useState('');
 const [rating, setRating] = useState(10);
 
 const handleTextChange = (e) => {
-  if (review.length === 0) {
+  if (text.length === 0) {
     setMessage(null)
     setisdIsabled(true)
-  } else if(review !== '' && review.trim().length <= 10) {
+  } else if(text !== '' && text.trim().length <= 10) {
     setMessage("Review must be at least 10 characters.")
     setisdIsabled(true)
   }else {
     setMessage(null)
     setisdIsabled(false)
   }
-  setReview(e.target.value)
-
+  setText(e.target.value)
 }
+
+const handleSubmit = e => {
+  e.preventDefault();
+  if (text.trim().length > 10) {
+    const newFeedback = {
+      rating,
+      text
+    }
+
+    handleAdd(newFeedback);
+    setText('');
+  }
+}
+
+
 
 const btnClasses = 'text-white absolute right-2.5 bottom-1.5 top-1.5 hover:bg-blue-800 focus:ring-2 focus:outline-none focus:ring-[#ff6a95]-300 font-medium rounded-lg text-md px-6 py-2';
 
   return (
     <div className="bg-white p-4 m-4 rounded-xl overflow-hidden">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2 className="font-bold text-center m-3 text-xl">
           How would be rate your service with us?
         </h2>
@@ -37,9 +51,9 @@ const btnClasses = 'text-white absolute right-2.5 bottom-1.5 top-1.5 hover:bg-bl
           <input
             type="text"
             onChange={handleTextChange}
-            value={review}
+            value={text}
             className="block p-2 pl-6 w-full text-lg text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-red-500 focus:border-blue-500"
-            placeholder="Write a review"
+            placeholder="Write a text"
             required
           />
           <button
